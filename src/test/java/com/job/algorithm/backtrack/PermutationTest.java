ca2686d524680c8a79d1cdbba95ea003e5dadb28
torychow <522617505@qq.com>
2018-05-27 01:53:04 +0800

    Add back track permutation

---
+++ src/test/java/com/job/algorithm/backtrack/PermutationTest.java
@@ -0,0 +1,11 @@
+package com.job.algorithm.backtrack;
+
+import org.junit.Test;
+
+public class PermutationTest {
+
+    @Test
+    public void test() {
+        Permutation.all_permutation("all");
+    }
+}
